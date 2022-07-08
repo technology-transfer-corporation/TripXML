5bfca20dfcfb1d74cea5c178ed88363425fa90e2
Alexander Kobelev <alex.kobelev@technologytransferusa.com>
2022-07-08 10:24:33 -0400

    ak

---
+++ src/wsTripXML/referenceUpdate.bat
@@ -0,0 +1,13 @@
+@echo off
+rem setlocal
+
+xcopy "..\TripXMLMain\Xsl\AmadeusWS\*.dll"  "bin"  /c /d /i /y
+xcopy "..\TripXMLMain\Xsl\BL\*.dll"  "bin"  /c /d /i /y
+xcopy "..\TripXMLMain\Xsl\Galileo\*.dll"  "bin"  /c /d /i /y
+xcopy "..\TripXMLMain\Xsl\Sabre\*.dll"  "bin"  /c /d /i /y
+xcopy "..\TripXMLMain\Xsl\TCML\*.dll"  "bin"  /c /d /i /y
+xcopy "..\TripXMLMain\Xsl\Travelport\*.dll"  "bin"  /c /d /i /y
+xcopy "..\TripXMLMain\Xsl\Worldspan\*.dll"  "bin"  /c /d /i /y
+
+rem del "*.dll" 
+pause
