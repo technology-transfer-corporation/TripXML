682055d41f09f826b9b14a777e8f88fd0c4901b8
Alexander Kobelev <alex.kobelev@technologytransferusa.com>
2023-07-25 11:57:57 -0400

    VCC service update

--- src/wsTripXML/My Project/AssemblyInfo.vb
+++ src/wsTripXML/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 <Assembly: AssemblyVersion("1.0.0.*")>
-<Assembly: AssemblyFileVersion("1.1.238.22")>
+<Assembly: AssemblyFileVersion("1.1.238.23")>
