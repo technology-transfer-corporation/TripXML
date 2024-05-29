3af87c286e73c0779aa4a0a258738185d2ec070e
Alexander Kobelev <alex.kobelev@technologytransferusa.com>
2024-05-29 14:12:29 -0400

    AK

--- src/wsTripXML/My Project/AssemblyInfo.vb
+++ src/wsTripXML/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 
 <Assembly: AssemblyVersion("1.0.0.*")>
-<Assembly: AssemblyFileVersion("1.1.238.96")>
+<Assembly: AssemblyFileVersion("1.1.239.3")>
