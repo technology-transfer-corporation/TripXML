eba621e98a7ad7ee00ed6563c839a61f64a1ade8
Alexander Kobelev <alex.kobelev@technologytransferusa.com>
2026-04-13 11:33:47 -0400

    Update wsTripXML submodule and .NET SDK path in batch file
    - Updated wsTripXML submodule to latest commit.
    - Changed SingleXSL.bat to use .NET Framework 4.8 SDK tools.
    - Updated v03_AmadeusWS_PNRReadRS.dll binary.
    - Noted dirty state in AdminTools, TripXMLMain, and XSLs submodules.

--- src/XSLs/AmadeusWS/SingleXSL.bat
+++ src/XSLs/AmadeusWS/SingleXSL.bat
@@ -1,6 +1,6 @@
 @echo off
 rem set /p id="Enter XSLT Name: "
-rem set path=C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools;%path%
+set path=C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools;%path%
 xsltc /settings:document+,script+ %~1%
 
 xcopy "*.dll"  "..\..\TripXMLMain\Xsl\AmadeusWS"  /c /d /i /y
